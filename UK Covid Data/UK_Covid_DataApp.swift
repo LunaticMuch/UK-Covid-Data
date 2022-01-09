5c6f3fc45d5f8e7b13f1e4c32ea0ccdd5dc069e1
LunaticMuch <s.cislaghi@gmail.com>
2022-01-09 17:48:27 +0000

    Initial Commit

---
+++ UK Covid Data/UK_Covid_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  UK_Covid_DataApp.swift
+//  UK Covid Data
+//
+//  Created by Stefano Cislaghi on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct UK_Covid_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
